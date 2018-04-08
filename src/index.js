96d4f98dcc6f6facd61912f6b79bfb2fa7c63a5a
MikeAngelo007 <matovaro@unal.edu.co>
2018-04-08 18:23:45 -0500

    Creada vista de entrega y modificado navbar de cada uno

--- src/index.js
+++ src/index.js
@@ -7,11 +7,19 @@ import App from "./mainLayout/App";
 // Pages
 import PageHome from './pages/PageHome'
 import PageLogin from './pages/PageLogin'
+import PageDeliver from './pages/pageDeliver';
+import AppDeliver from './mainLayout/AppDeliver';
+import AppLogin from './mainLayout/AppLogin';
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App} >
       <IndexRoute component={PageHome}/>
+    </Route>
+    <Route path="/" component={AppDeliver} >
+      <Route path="entregar" component={PageDeliver}/>
+    </Route>
+    <Route path="/" component={AppLogin} >
       <Route path="login" component={PageLogin}/>
     </Route>
   </Router>
