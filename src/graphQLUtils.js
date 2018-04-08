ac781fc1c6c73ebb7c5c62b17cd66742828d1cd7
Juan Carlos <jcgamar@unal.edu.co>
2018-04-08 17:47:33 -0500

    funcion para GraphQL

---
+++ src/graphQLUtils.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+/**
+ * 
+ * @param {String} requestText String con la petición
+ * @param {function} handleResponse función para manejar la respuesta de graphQL devuelve un objeto con formato GraphQL
+ * @param {function} handleError función para manejar la respuesta de graphQL devuelve un objeto con formato GraphQL
+ */
+export default function GraphQLRequest(requestText, handleResponse, handleError){
+
+  var GQl = JSON.stringify({
+    query: requestText
+  });
+
+  axios({
+    headers: { 'Content-Type': 'application/json' },
+    url: "http://localhost:5000/graphql",
+    method: "POST",
+    data: GQl,
+    responseType: 'json'
+  })
+  .then(function (response) {
+    if(!response.error && handleResponse)
+      handleResponse(response.data.data);
+  }).catch(function (error){
+    if (error.response && handleError)
+      handleError(error.response.status, error.response.data.errors);
+  });
+};
