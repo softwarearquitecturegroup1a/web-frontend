333e980d374ae1ccf91cf062366113092e29a9c3
Miguel Tovar <matovaro@unal.edu.co>
2018-04-17 00:36:57 -0500

    Configuracion Request

--- src/pages/pageRequest.js
+++ src/pages/pageRequest.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import GlRequest from '../graphQLUtils';
+import cookie from 'react-cookies'
 
 class Request extends Component{
     constructor(props){
@@ -8,7 +10,8 @@ class Request extends Component{
             end:''
         };
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleEndChange = this.handleEndChange.bind(this);
+        this.handleStartChange = this.handleStartChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -26,22 +29,72 @@ class Request extends Component{
         event.preventDefault();
         const start = this.state.start;
         const end = this.state.end;
+
+        var getbike = `query{
+            bicicletaById(serial: 21325456){
+              serial
+            }
+          }`
+
+        GlRequest(getbike,
+        (data)=> {
+            cookie.save('bicicleta', data.bicicletaById.serial, { path: '/', });
+        }, 
+        (status, data) => {
+          
+        });
+
+        var updbike = `mutation{
+            updateBicicleta(serial: ${cookie.load('bicicleta')}, bicicleta:{
+              estado: "Ocupada"
+              ubicacion: ${end}
+            }){
+              estado
+              ubicacion
+            }
+        }`
+
+        GlRequest(updbike,
+            (data)=> {
+                cookie.save('estado', data.updateBicicleta.estado, { path: '/', });
+                cookie.save('ubicacion', data.updateBicicleta.ubicacion, { path: '/', });
+            }, 
+            (status, data) => {
+              
+            });
+
+        var insertion = `mutation{
+            createPrestamo(prestamo:{
+              student_id: ${cookie.load('identification')}
+              bici_id: ${cookie.load('bicicleta')}
+              solicitud: ${new Date()}
+            }){
+              solicitud
+            }
+          
+        }`
+
+        GlRequest(insertion,
+            (data)=> {
+                cookie.save('solicitud', data.createPrestamo.solicitud, { path: '/', });
+            }, 
+            (status, data) => {
+              
+            });
+        
+        var bicid = cookie.load('bicicleta')
+
+        
     }
-}
-class PageRequest extends Component {
-    render() {
-        return (
-            <section id="request" style={{ "paddingTop": "calc(6rem + 72px)" }} >
-                <div class="container">
-                    <h2 class="text-center text-uppercase text-secondary mb-0">Inicia tu viaje</h2>
-                    <hr class="star-dark mb-5" />
-                    <div class="row">
-                    <div class="col-lg-8 mx-auto">
-                        <form name="sentMessage" id="contactForm" novalidate="novalidate">
+
+    render(){
+        return(
+
+            <form name="sentMessage" id="contactForm" novalidate="novalidate" onSubmit={this.handleSubmit.bind(this)} action="/">
                         <div class="control-group">
                             <div class="form-group floating-label-form-group controls mb-0 pb-2 text-center">
                             <h5 class="text-center">Estacion de inicio</h5>
-                            <select class="form-control text-center" id="start" required="required">
+                            <select class="form-control text-center" value={this.state.start} id="start" onChange={this.handleStartChange.bind(this)} required="required">
                                 <option>Edificio CyT</option>
                                 <option>Entrada Calle 26</option>
                                 <option>Entrada Edificio Uriel Gutierrez</option>
@@ -54,7 +107,7 @@ class PageRequest extends Component {
                         <div class="control-group">
                             <div class="form-group floating-label-form-group controls mb-0 pb-2 text-center">
                             <h5 class="text-center">Estacion de destino</h5>
-                            <select class="form-control text-center" id="end" required="required">
+                            <select class="form-control text-center" value={this.state.end} id="end" onChange={this.handleEndChange.bind(this)} required="required">
                                 <option>Edificio CyT</option>
                                 <option>Entrada Calle 26</option>
                                 <option>Entrada Edificio Uriel Gutierrez</option>
@@ -67,13 +120,53 @@ class PageRequest extends Component {
                         <br />
                         <div id="success"></div>
                         <div class="form-group text-center">
+                        <a class="portfolio-item d-block mx-auto" href="#bicishow">
                             <button type="submit" class="btn btn-primary btn-xl" id="sendMessageButton">Solicitar</button>
+                        </a>
                         </div>
                         </form>
+
+        )
+    }
+}
+class PageRequest extends Component {
+    render() {
+        return (
+            <section id="request" style={{ "paddingTop": "calc(6rem + 72px)" }} >
+                <div class="container">
+                    <h2 class="text-center text-uppercase text-secondary mb-0">Inicia tu viaje</h2>
+                    <hr class="star-dark mb-5" />
+                    <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                        <Request />
                     </div>
                     </div>
                 </div>
+                
+
+                <div class="portfolio-modal mfp-hide" id="bicishow">
+                <div class="portfolio-modal-dialog bg-white">
+                    <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+                         <i class="fa fa-3x fa-times"></i>
+                    </a>
+                    <div class="container text-center">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <h2 class="text-secondary text-uppercase mb-0">Tu bici es la:</h2>
+                                <hr class="star-dark mb-5" /> 
+                                    <p class="mb-5"> <bicid /> </p>
+                                <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                                <i class="fa fa-close"></i>
+                                ¡Vamos!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                
             </section>
+
+            
 
 
 
